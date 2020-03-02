ee21c29dd7e44a516fc0e07efae9754496b44660
Jay <18782919648@163.com>
2020-03-02 11:38:13 +0000

    Add score UI and Score controller In HP_controller

--- Assets/HP_Controller.cs
+++ Assets/HP_Controller.cs
@@ -7,16 +7,20 @@ using UnityEngine.Events;
 public class HP_Controller : MonoBehaviour
 {
     public Slider Slider;
+    static public int ScoreInt;
+    static private int scoreInt;
+    public static Text txt;
+
     // Start is called before the first frame update
     void Start()
     {
        GameObject btnObj = GameObject.Find ("Ocean/Boat/HP_Canvas/Add");
        GameObject btnObj_d = GameObject.Find ("Ocean/Boat/HP_Canvas/De");
-       Button btn = (Button) btnObj.GetComponent<Button>();
+       Button btn   = (Button) btnObj.GetComponent<Button>();
        Button btn_d = (Button) btnObj_d.GetComponent<Button>();
        Slider.value = 100;
-       btn.onClick.AddListener (HP_increase);
-       btn_d.onClick.AddListener (HP_decrease);
+       btn.onClick.AddListener(HP_increase());
+       btn_d.onClick.AddListener(HP_decrease());
     }
 
    // Update is called once per frame
@@ -26,10 +30,17 @@ public class HP_Controller : MonoBehaviour
    }
 
    public void HP_decrease(){
-        Slider.value -= 20;
+        txt = GameObject.Find("Score").GetComponent<Text>();
+        scoreInt -= 10;
+        txt.text = scoreInt.ToString();
+
    }
    public void HP_increase(){
-        Slider.value += 20;
+        txt = GameObject.Find("Score").GetComponent<Text>();
+        scoreInt += 10;
+        txt.text = scoreInt.ToString();
+
+
    }
 
 }
